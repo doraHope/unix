0ce35ad7e326fec643554bd89390ca07b75622f6
dora_Miku@163.com <dora_Miku@163.com>
2019-12-01 21:09:25 +0800

    回射程序客服端升级v1.1

--- ch6_ser_client/client.c
+++ ch6_ser_client/client.c
@@ -1,36 +1,161 @@
+//回射函数-客服端v1.1(使用select 监听 可读的描述字 + 增强了对tcp读操作的处理)
 #include "../lib/dora.h"
 
-int contract(int cfd, int ifd)
+int contract1(int cfd, int ifd)
 {
     int rcnt = 0;
     int ofd = 1;
     char buffer[MAX_CHAR_LEN];
     char recv[MAX_CHAR_LEN];
     //首先读取
-
-    bzero(&buffer, 0);
-    bzero(&recv, 0);
-    if ((rcnt = Read(ifd, buffer, MAX_CHAR_LEN)) >= 0)
+    while (1)
     {
-        if (0 == rcnt)
+        bzero(&buffer, 0);
+        bzero(&recv, 0);
+        if ((rcnt = Read(ifd, buffer, MAX_CHAR_LEN)) >= 0)
         {
-            printf("input line end!\n");
-            return; //结束符
+            if (0 == rcnt)
+            {
+                printf("input line end!\n");
+                return; //结束符
+            }
+            printf("len:`%d`, cli-input:%s", strlen(buffer), buffer);
+            writen(cfd, buffer, strlen(buffer));
+            if (0 == (rcnt = Read(cfd, recv, MAX_CHAR_LEN)))
+            { //FIN
+                printf("server send FIN\n");
+                return;
+            }
+            printf("server-ret: read:%s", recv);
         }
-        printf("len:`%d`, cli-input:%s", strlen(buffer), buffer);
-        writen(cfd, buffer, strlen(buffer));
-        if (0 == (rcnt = Read(cfd, recv, MAX_CHAR_LEN)))
-        { //FIN
-            printf("server send FIN\n");
+        else
+        {
+            printf("read failed!\n");
             return;
         }
-        printf("server-ret: read:%s", recv);
     }
-    else
+}
+
+int contract2(int cfd, int ifd)
+{
+    int rcnt = 0;
+    int ofd = 1;
+    char buffer[MAX_CHAR_LEN];
+    char recv[MAX_CHAR_LEN];
+    //首先读取
+    while (1)
     {
-        printf("read failed!\n");
-        return;
+        bzero(&buffer, 0);
+        bzero(&recv, 0);
+        fd_set read_set;
+        FD_ZERO(&read_set);
+        FD_SET(ifd, &read_set);
+        FD_SET(cfd, &read_set);
+        int maxfd = max(ifd, cfd) + 1;
+        int fret = 0;
+        Select(maxfd, &read_set, NULL, NULL, NULL);
+        if (FD_ISSET(cfd, &read_set))
+        {
+            if (0 == (fret = Read(cfd, buffer, MAX_CHAR_LEN)))
+            {
+                return;
+            }
+            if (fret < 0)
+            {
+                printf("read fail! error:`%d`, error:`%s`\n", errno, error);
+            }
+            printf("server-ret:%s", buffer);
+        }
+        if (FD_ISSET(ifd, &read_set))
+        {
+            if (0 == (fret = Read(ifd, recv, MAX_CHAR_LEN)))
+            {
+                return; //EOF
+            }
+            if (fret < 0)
+            {
+                printf("read fail! error:`%d`, error:`%s`\n", errno, error);
+            }
+            if (0 > writen(cfd, recv, strlen(recv)))
+            {
+                printf("contract2 write fail! errno:`%d`, error:`%s`\n", errno, error);
+            }
+        }
     }
+}
+
+int contract3(int cfd, int ifd)
+{
+    int rcnt = 0;
+    int ofd = 1;
+    char buffer[MAX_CHAR_LEN];
+    char recv[MAX_CHAR_LEN];
+    // char dt[] = "haaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaahahahahahahahahahahahahahahahahahahahahahhaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaahahahahahahahahahahahahahahahahahahahahahhaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaahahahahahahahahahahahahahahahahahahahahahhaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaahahahahahahahahahahahahahahahahahahahahahhaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaahahahahahahahahahahahahahahahahahahahahahhaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaahahahahahahahahahahahahahahahahahahahahahhaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaahahahahahahahahahahahahahahahahahahahahahhaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaahahahahahahahahahahahahahahahahahahahahahhaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaahahahahahahahahahahahahahahahahahahahahahhaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaahahahahahahahahahahahahahahahahahahahahahhaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaahahahahahahahahahahahahahahahahahahahahahhaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaahahahahahahahahahahahahahahahahahahahahahhaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaahahahahahahahahahahahahahahahahahahahahahhaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaahahahahahahahahahahahahahahahahahahahahahhaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaahahahahahahahahahahahahahahahahahahahahahhaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaahahahahahahahahahahahahahahahahahahahahahhaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaahahahahahahahahahahahahahahahahahahahahahhaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaahahahahahahahahahahahahahahahahahahahahahhaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaahahahahahahahahahahahahahahahahahahahahahhaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaahahahahahahahahahahahahahahahahahahahahahhaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaahahahahahahahahahahahahahahahahahahahahahhaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaahahahahahahahahahahahahahahahahahahahahahhaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaahahahahahahahahahahahahahahahahahahahahahhaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaahahahahahahahahahahahahahahahahahahahahahhaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaahahahahahahahahahahahahahahahahahahahahahhaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaahahahahahahahahahahahahahahahahahahahahahhaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaahahahahahahahahahahahahahahahahahahahahahhaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaahahahahahahahahahahahahahahahahahahahahahhaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaahahahahahahahahahahahahahahahahahaahahahahahahhaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaahahahahahahahahahahahahahahahahahahahahah\n";
+    int flag_input_end = 0;
+    while (1)
+    {
+        memset(&recv, 0, MAX_CHAR_LEN * sizeof(char));
+        memset(&buffer, 0, MAX_CHAR_LEN * sizeof(char));
+        fd_set read_set;
+        FD_ZERO(&read_set);
+        FD_SET(ifd, &read_set);
+        FD_SET(cfd, &read_set);
+        int maxfd = max(ifd, cfd) + 1;
+        int fret = 0;
+        Select(maxfd, &read_set, NULL, NULL, NULL);
+        if (FD_ISSET(cfd, &read_set))
+        {
+            if (0 == (fret = Read(cfd, buffer, MAX_CHAR_LEN)))
+            {
+                if (1 == flag_input_end)
+                {
+                    //正常情况下关闭的状态
+                    printf("slef-close: read fin!\n");
+                    return 0;
+                }
+                else
+                {
+                    //服务器关闭的状态
+                    printf("type end!");
+                    sys_quit("server closed!");
+                }
+            }
+            if (fret < 0)
+            {
+                printf("read fail! error:`%d`, error:`%s`\n", errno, error);
+            }
+            printf("server-ret:%s", buffer);
+        }
+        if (FD_ISSET(ifd, &read_set))
+        {
+            if (0 == (fret = Read(ifd, recv, MAX_CHAR_LEN)))
+            {
+                printf("input end\n");
+                flag_input_end = 1;
+                Shutdown(cfd, SHUT_WR);
+                // FD_CLR(ifd, &read_set);
+                continue;
+            }
+            if (fret < 0)
+            {
+                printf("read fail! error:`%d`, error:`%s`\n", errno, error);
+            }
+            /*------------------------------------ 服务端 进行测试*/
+            // int i = 0;
+            // for(; i < 10; i++) {
+            //     if (0 > writen(cfd, dt, strlen(dt)))
+            //     {
+            //         printf("contract write fail! errno:`%d`, error:`%s`\n", errno, error);
+            //     }
+            // }
+            /*--- */
+            if (0 > writen(cfd, cfd, strlen(dt)))
+            {
+                printf("contract write fail! errno:`%d`, error:`%s`\n", errno, error);
+            }
+        }
+    }
+    return 0;
 }
 
 int main(int argc, char **argv)
@@ -60,6 +185,6 @@ int main(int argc, char **argv)
         printf("连接失败, error:`%s`", error);
         exit(0);
     }
-    contract(cfd, ifd);
+    contract3(cfd, ifd);
     close(cfd);
 }
